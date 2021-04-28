06023caea7adb7f548282a3ee8d79b9c744bbac0
toriwasa <toriwasa.py@gmail.com>
2021-04-29 03:10:56 +0900

    :+1: keymap追加

---
+++ keyboards/crkbd/rev1/common/keymaps/toriwasa/rules.mk
@@ -0,0 +1,6 @@
+MOUSEKEY_ENABLE     = yes     # Mouse keys
+EXTRAKEY_ENABLE = yes  # audio keys
+RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes         # Enable VIA
+OLED_DRIVER_ENABLE  = yes
+LTO_ENABLE          = yes
